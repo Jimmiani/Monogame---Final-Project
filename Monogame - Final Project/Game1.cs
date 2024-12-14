458cbfef09f5325fcfab5e14f6cd0b0aa2c9be79
Emmett Cornies <339761330@GoTVDSB.ca>
2024-12-13 22:48:07 -0500

    checkpoint

--- Monogame - Final Project/Game1.cs
+++ Monogame - Final Project/Game1.cs
@@ -11,22 +11,27 @@ namespace Monogame___Final_Project
         private SpriteBatch _spriteBatch;
 
         Rectangle window;
+        MouseState mouseState, prevMouseState;
 
 
         // Audio
         Song hauntedHouseSong;
 
         // Backgrounds
-        Texture2D introTexture;
+        Texture2D introTexture, forestTexture;
 
         // Buttons
         Texture2D playBtnTexture;
         Rectangle playBtnRect;
 
+        // Fonts
+        SpriteFont titleFont;
+
 
         enum Screen
         {
             Intro,
+            Forest,
 
         }
         Screen screen;
@@ -47,7 +52,7 @@ namespace Monogame___Final_Project
 
             base.Initialize();
 
-            playBtnRect = new Rectangle((window.Width / 2) - (playBtnTexture.Width / 2), 300, playBtnTexture.Width, playBtnTexture.Height);
+            playBtnRect = new Rectangle((window.Width / 2) - (playBtnTexture.Width / 2), 350, playBtnTexture.Width, playBtnTexture.Height);
         }
 
         protected override void LoadContent()
@@ -59,9 +64,13 @@ namespace Monogame___Final_Project
 
             // Backgrounds
             introTexture = Content.Load<Texture2D>("Backgrounds/hauntedIntro");
+            forestTexture = Content.Load<Texture2D>("Backgrounds/forestBackground");
 
             // Buttons
             playBtnTexture = Content.Load<Texture2D>("Buttons/playBtn");
+
+            // Fonts
+            titleFont = Content.Load<SpriteFont>("Fonts/pixelFont");
         }
 
         protected override void Update(GameTime gameTime)
@@ -69,9 +78,37 @@ namespace Monogame___Final_Project
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            prevMouseState = mouseState;
+            mouseState = Mouse.GetState();
+            this.Window.Title = $"x = {mouseState.X}, y = {mouseState.Y}";
+
             if (MediaPlayer.State == MediaState.Stopped)
             {
                 MediaPlayer.Play(hauntedHouseSong);
+            }
+
+            if (screen == Screen.Intro)
+            {
+                if (playBtnRect.Contains(mouseState.Position))
+                {
+                    playBtnRect = new Rectangle(((window.Width / 2) - (playBtnTexture.Width / 2)) - 5, 345, playBtnTexture.Width + 10, playBtnTexture.Height + 8);
+                    if (prevMouseState.LeftButton == ButtonState.Pressed)
+                    {
+                        playBtnRect = new Rectangle(((window.Width / 2) - (playBtnTexture.Width / 2)) + 5, 355, playBtnTexture.Width - 10, playBtnTexture.Height - 8);
+                        if (mouseState.LeftButton == ButtonState.Released)
+                        {
+                            screen = Screen.Forest;
+                            playBtnRect = new Rectangle((window.Width / 2) - (playBtnTexture.Width / 2), 350, playBtnTexture.Width, playBtnTexture.Height);
+                        }
+                    }
+                }
+                else if (!playBtnRect.Contains(mouseState.Position))
+                {
+                    if (prevMouseState.LeftButton == ButtonState.Released)
+                    {
+                        playBtnRect = new Rectangle((window.Width / 2) - (playBtnTexture.Width / 2), 350, playBtnTexture.Width, playBtnTexture.Height);
+                    }
+                }
             }
 
             base.Update(gameTime);
@@ -88,6 +125,12 @@ namespace Monogame___Final_Project
             {
                 _spriteBatch.Draw(introTexture, new Vector2(0, 0), Color.White);
                 _spriteBatch.Draw(playBtnTexture, playBtnRect, Color.White);
+                _spriteBatch.DrawString(titleFont, "The Elder", new Vector2(20, 20), Color.DimGray, 0, new Vector2(0, 0), 0.6f, SpriteEffects.None, 0);
+                _spriteBatch.DrawString(titleFont, "Gloom", new Vector2(190, 120), Color.ForestGreen, 0, new Vector2(0, 0), 1, SpriteEffects.None, 0);
+            }
+            else if (screen == Screen.Forest)
+            {
+                _spriteBatch.Draw(forestTexture, new Vector2(0, 0), Color.White);
             }
 
 
